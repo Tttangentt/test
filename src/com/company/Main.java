960ff4c76260638131ee8517e54e4d3c8872bee8
Rawis <tantai.tt29@gmail.com>
2020-10-20 15:13:22 +0700

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+
+
+}
